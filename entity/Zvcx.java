72dc2ba645c54ffcc88101dc81c4caeffb2706f5
JeevaVj3 <famousjeeva@gmail.com>
2023-11-08 12:33:15 +0530

    updated one

---
+++ entity/Zvcx.java
@@ -0,0 +1,56 @@
+package com.onesoft.supercar.entity;
+
+public class Zvcx {
+	
+	private int id;
+	private String brand;
+	private int price;
+	private int cc;
+	private String colour;
+	private boolean ispetrol;
+	private int milage;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getBrand() {
+		return brand;
+	}
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public int getCc() {
+		return cc;
+	}
+	public void setCc(int cc) {
+		this.cc = cc;
+	}
+	public String getColour() {
+		return colour;
+	}
+	public void setColour(String colour) {
+		this.colour = colour;
+	}
+	public boolean isIspetrol() {
+		return ispetrol;
+	}
+	public void setIspetrol(boolean ispetrol) {
+		this.ispetrol = ispetrol;
+	}
+	public int getMilage() {
+		return milage;
+	}
+	public void setMilage(int milage) {
+		this.milage = milage;
+	}
+
+}
